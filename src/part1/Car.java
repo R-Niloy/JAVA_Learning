e7f623e331805adb396d96dd36a53690a1be009e
R-Niloy <43363781+R-Niloy@users.noreply.github.com>
2021-01-20 05:58:08 -0500

    Update: January 20th

---
+++ src/part1/Car.java
@@ -0,0 +1,17 @@
+package part1;
+
+public class Car {
+    private int door;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+    public void setModel(String model){
+        this.model=model;
+    }
+    public String getModel(){
+        return this.model;
+
+    }
+}
---
+++ src/part1/main.java
@@ -0,0 +1,14 @@
+package part1;
+
+public class main {
+    public static void main(String[] args) {
+        Car toyota = new Car();
+
+        Car porsche = new Car();
+        toyota.setModel("Corolla");
+        System.out.println("The toyota model is : "+ toyota.getModel());
+
+
+    }
+
+}
